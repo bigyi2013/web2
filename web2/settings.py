750c594d3c4549f593a73eeb801560aec68e6b66
bigyi2013 <sun163sun163@126.com>
2018-08-27 09:55:42 +0800

    debug

--- web2/settings.py
+++ web2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o4dimy#o-@*@crg%fvuwe7+k7m6s4_^mg3-2h$7*kb#f_p3(2('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
