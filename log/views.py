6340e997cf8221b4d73849e0fd042e53c702782f
bigyi2013 <sun163sun163@126.com>
2018-09-16 16:46:39 +0800

    1

--- log/views.py
+++ log/views.py
@@ -41,24 +41,6 @@ def comment(request):
             comment.save()
 
 
-        # else:
-        #     # 检查到数据不合法，重新渲染详情页，并且渲染表单的错误。
-        #     # 因此我们传了三个模板变量给 detail.html，
-        #     # 一个是文章（Post），一个是评论列表，一个是表单 form
-        #     # 注意这里我们用到了 post.comment_set.all() 方法，
-        #     # 这个用法有点类似于 Post.objects.all()
-        #     # 其作用是获取这篇 post 下的的全部评论，
-        #     # 因为 Post 和 Comment 是 ForeignKey 关联的，
-        #     # 因此使用 post.comment_set.all() 反向查询全部评论。
-        #     # 具体请看下面的讲解。
-        #     comment_list = comment_article.comment_set.all()
-        #     context = {'article': comment_article,
-        #                'form': form,
-        #                'comment_list': comment_list
-        #                }
-        #     return render(request, 'blog/article_detail.html', context=context)
-    # 不是 post 请求，说明用户没有提交数据，重定向到文章详情页。
-
     return render(request, 'log/comment.html', context)
 
 # Create your views here.
